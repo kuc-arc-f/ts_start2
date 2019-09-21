acf72b4868ed97cc5a353d9f49db5edba1a72182
kuc-arc-f <naka@kuc-arc-f.com>
2019-09-21 15:28:03 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+﻿# ts_start2
+
+ Version: 0.9.1
+
+ Author  : Kouji Nakashima / kuc-arc-f.com
+
+ date    : 2019/09/21
+
+***
+TypeScript sample, tsconfig.json use
+
+***
+### blog
+
+***
+
---
+++ greeter1.ts
@@ -0,0 +1,12 @@
+//
+
+
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+let user = "Jane User112244fff";
+
+console.log( greeter(user) );
+document.body.textContent = greeter(user);
+
